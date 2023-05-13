b6df5c3e4fdcad8202619b81a6cbbb2317707f46
JesseSinivuori <sinvuorii@gmail.com>
2023-05-13 13:48:06 +0300

    1.5: kurssitiedot step5

--- osa1/part1/src/App.tsx
+++ osa1/part1/src/App.tsx
@@ -4,21 +4,23 @@ type Part = {
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   const Header = (props: { course: string }) => <h1>{props.course}</h1>;
 
@@ -56,9 +58,9 @@ const App = () => {
 
   return (
     <>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </>
   );
 };
