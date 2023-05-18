a03286a8fefeaf4012306931be06cf4a47e461c9
JesseSinivuori <sinvuorii@gmail.com>
2023-05-18 17:05:32 +0300

    2.3*: kurssitiedot step8

--- osa2/kurssitiedot/src/components/Total.tsx
+++ osa2/kurssitiedot/src/components/Total.tsx
@@ -2,11 +2,11 @@ import { Course } from "../App";
 
 const Total = ({ course }: { course: Course }) => {
   const { parts } = course;
-  let sumOfExercices = 0;
-  parts.forEach((part) => {
-    sumOfExercices += part.exercises;
-  });
-  return <b>Total of {sumOfExercices} exercises</b>;
+  const totalExercises = parts.reduce(
+    (total, part) => total + part.exercises,
+    0
+  );
+  return <b>Total of {totalExercises} exercises</b>;
 };
 
 export default Total;
