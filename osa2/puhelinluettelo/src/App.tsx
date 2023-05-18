99db50728969fd90e38414fc7f70d702d0b7fb8b
JesseSinivuori <sinvuorii@gmail.com>
2023-05-18 19:11:07 +0300

    2.8: puhelinluettelo step3

--- osa2/puhelinluettelo/src/App.tsx
+++ osa2/puhelinluettelo/src/App.tsx
@@ -2,19 +2,28 @@ import { useState } from "react";
 
 export type Person = {
   name: string;
+  number?: string;
 };
 
-const Person = ({ person }: { person: Person }) => <p>{person.name}</p>;
+const Person = ({ person }: { person: Person }) => (
+  <p>
+    {person.name} {person.number}
+  </p>
+);
 
 const App = () => {
-  const [persons, setPersons] = useState<Person[]>([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState<Person[]>([
+    { name: "Arto Hellas", number: "+3581234567" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const newPerson = {
       name: newName,
+      number: newNumber,
     };
 
     if (persons.some((person) => person.name === newPerson.name)) {
@@ -30,12 +39,19 @@ const App = () => {
     setNewName(e.target.value);
   };
 
+  const handleNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewNumber(e.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
