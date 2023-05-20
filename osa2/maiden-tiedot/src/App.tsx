e5d3f5a894be4bb70b0ef02be30cad41c2e9e9ed
JesseSinivuori <sinvuorii@gmail.com>
2023-05-21 01:43:17 +0300

    2.19*: maiden tiedot, step2

--- osa2/maiden-tiedot/src/App.tsx
+++ osa2/maiden-tiedot/src/App.tsx
@@ -16,18 +16,25 @@ export type CountryT = {
 
 function App() {
   const [countries, setCountries] = useState<CountryT[]>([]);
-  const [filter, setFilter] = useState("");
+  const [filter, setFilter] = useState("fi");
+  const [selectedCountryName, setSelectedCountryName] = useState("finland");
   const filteredCountries = countries.filter((country) =>
     country.name.common.toLowerCase().includes(filter.toLowerCase())
   );
+  const selectedCountry = filteredCountries.find(
+    (country) => country.name.common.toLowerCase() === selectedCountryName
+  );
 
-  console.log(filteredCountries);
   useEffect(() => {
     const response = axios.get(`http://localhost:3001/countries`); //https://restcountries.com/v3.1/all
     response
       .then((response) => response.data)
       .then((countries) => setCountries(countries));
   }, []);
+
+  useEffect(() => {
+    setSelectedCountryName("");
+  }, [filter]);
 
   return (
     <div>
@@ -37,20 +44,29 @@ function App() {
         value={filter}
         onChange={(e) => setFilter(e.target.value)}
       />
-      {filter !== "" && (
-        <ul>
+      {filter !== "" && selectedCountryName === "" && (
+        <div>
           {filteredCountries.length < 10 ? (
             filteredCountries.map((country) => (
               <Country
                 key={country.name.common}
                 country={country}
                 expand={filteredCountries.length === 1}
+                setSelectedCountryName={setSelectedCountryName}
               />
             ))
           ) : (
             <p>Too many matches, specify another filter</p>
           )}
-        </ul>
+        </div>
+      )}
+      {selectedCountry && (
+        <Country
+          key={selectedCountry.name.common}
+          country={selectedCountry}
+          expand={true}
+          setSelectedCountryName={setSelectedCountryName}
+        />
       )}
     </div>
   );
--- osa2/maiden-tiedot/src/components/Country.tsx
+++ osa2/maiden-tiedot/src/components/Country.tsx
@@ -1,17 +1,32 @@
+import { SetStateAction } from "react";
 import { CountryT } from "../App";
 
 const Country = ({
   country,
   expand,
+  setSelectedCountryName,
 }: {
   country: CountryT;
   expand: boolean;
+  setSelectedCountryName: React.Dispatch<SetStateAction<string>>;
 }) => {
+  const handleShow = () => {
+    setSelectedCountryName(country.name.common.toLowerCase());
+  };
+
   return (
     <div>
-      {!expand ? (
-        <p>{country.name.common}</p>
-      ) : (
+      {!expand && (
+        <div>
+          <p>
+            {country.name.common}
+            <button type="button" onClick={handleShow}>
+              show
+            </button>
+          </p>
+        </div>
+      )}
+      {expand && (
         <>
           <h1>{country.name.common}</h1>
           <p>capital: {country.capital}</p>
