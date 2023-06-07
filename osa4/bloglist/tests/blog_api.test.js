3959efd4f12b1dac041267e9599041f99166ac6f
JesseSinivuori <sinvuorii@gmail.com>
2023-06-07 14:32:01 +0300

    4.13 blogilistan laajennus, step1

--- osa4/bloglist/tests/blog_api.test.js
+++ osa4/bloglist/tests/blog_api.test.js
@@ -3,100 +3,119 @@ const supertest = require("supertest");
 const app = require("../app");
 const Blog = require("../models/blog");
 const api = supertest(app);
-const { initialBlogs, nonExistingId, blogsInDb } = require("./test_helper");
+const { initialBlogs, blogsInDb } = require("./test_helper");
 
 beforeEach(async () => {
   await Blog.deleteMany({});
   await Blog.insertMany(initialBlogs);
 });
 
-test("blogs are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
+describe("when there's initially some blogs saved", () => {
+  test("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("all blogs are returned", async () => {
+    const response = await api.get("/api/blogs");
+
+    expect(response.body).toHaveLength(initialBlogs.length);
+  });
+
+  test("a specific blog is within the returned blogs", async () => {
+    const response = await api.get("/api/blogs");
+
+    const titles = response.body.map((r) => r.title);
+    expect(titles).toContain("React patterns");
+  });
 });
 
-test("all blogs are returned", async () => {
-  const response = await api.get("/api/blogs");
+describe("addition of a new blog", () => {
+  test("a valid blog can be added", async () => {
+    const newBlog = {
+      title: "New Title",
+      author: "Author",
+      url: "url",
+      likes: 0,
+    };
 
-  expect(response.body).toHaveLength(initialBlogs.length);
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const blogsAtEnd = await blogsInDb();
+
+    const titles = blogsAtEnd.map((r) => r.title);
+
+    expect(titles).toHaveLength(initialBlogs.length + 1);
+    expect(titles).toContain("New Title");
+  });
+
+  test("fails with status code 400 if there's no title", async () => {
+    const newBlogwithoutTitle = {
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+    };
+
+    await api.post("/api/blogs").send(newBlogwithoutTitle).expect(400);
+
+    const blogsAtEnd = await blogsInDb();
+
+    expect(blogsAtEnd).toHaveLength(initialBlogs.length);
+  });
+
+  test("fails with status code 400 if there's no url", async () => {
+    const newBlogwithoutUrl = {
+      title: "Title",
+      author: "Edsger W. Dijkstra",
+      likes: 5,
+    };
+
+    await api.post("/api/blogs").send(newBlogwithoutUrl).expect(400);
+
+    const blogsAtEnd = await blogsInDb();
+
+    expect(blogsAtEnd).toHaveLength(initialBlogs.length);
+  });
+
+  test("if likes is not set, it will be 0", async () => {
+    const newBlog = {
+      title: "New Title",
+      author: "Author",
+      url: "url",
+    };
+    await api.post("/api/blogs").send(newBlog);
+    const blogsAtEnd = await blogsInDb();
+    const savedBlog = blogsAtEnd[blogsAtEnd.length - 1];
+
+    expect(savedBlog.likes).toEqual(0);
+  });
 });
 
-test("a specific blog is within the returned blogs", async () => {
-  const response = await api.get("/api/blogs");
-
-  const titles = response.body.map((r) => r.title);
-  expect(titles).toContain("React patterns");
+describe("blogs", () => {
+  test("have an id property", async () => {
+    const blogs = await blogsInDb();
+    const blogsIds = blogs.map((blog) => blog.id);
+    expect(blogsIds).toBeDefined();
+  });
 });
 
-test("a valid blog can be added", async () => {
-  const newBlog = {
-    title: "New Title",
-    author: "Author",
-    url: "url",
-    likes: 0,
-  };
+describe("deletion of a blog", () => {
+  test("succeeds with status code 204 if id is valid", async () => {
+    const blogs = await blogsInDb();
+    const blogToDelete = blogs[blogs.length - 1];
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+  });
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
-
-  const blogsAtEnd = await blogsInDb();
-
-  const titles = blogsAtEnd.map((r) => r.title);
-
-  expect(titles).toHaveLength(initialBlogs.length + 1);
-  expect(titles).toContain("New Title");
-});
-
-test("blog without title is not added", async () => {
-  const newBlogwithoutTitle = {
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-  };
-
-  await api.post("/api/blogs").send(newBlogwithoutTitle).expect(400);
-
-  const blogsAtEnd = await blogsInDb();
-
-  expect(blogsAtEnd).toHaveLength(initialBlogs.length);
-});
-
-test("blog without url is not added", async () => {
-  const newBlogwithoutUrl = {
-    title: "Title",
-    author: "Edsger W. Dijkstra",
-    likes: 5,
-  };
-
-  await api.post("/api/blogs").send(newBlogwithoutUrl).expect(400);
-
-  const blogsAtEnd = await blogsInDb();
-
-  expect(blogsAtEnd).toHaveLength(initialBlogs.length);
-});
-
-test("blogs have id property", async () => {
-  const blogs = await blogsInDb();
-  const blogsIds = blogs.map((blog) => blog.id);
-  expect(blogsIds).toBeDefined();
-});
-
-test("if likes is not set, it will be 0", async () => {
-  const newBlog = {
-    title: "New Title",
-    author: "Author",
-    url: "url",
-  };
-  await api.post("/api/blogs").send(newBlog);
-  const blogsAtEnd = await blogsInDb();
-  const savedBlog = blogsAtEnd[blogsAtEnd.length - 1];
-
-  expect(savedBlog.likes).toEqual(0);
+  test("fails with status code 400 if id is not valid", async () => {
+    const NonValidId = "fakeid";
+    await api.delete(`/api/blogs/${NonValidId}`).expect(400);
+  });
 });
 
 afterAll(async () => {
--- osa4/bloglist/tests/test_helper.js
+++ osa4/bloglist/tests/test_helper.js
@@ -19,17 +19,9 @@ const initialBlogs = [
   },
 ];
 
-const nonExistingId = async () => {
-  const blog = new Blog({ title: "willremovethissoon" });
-  await blog.save();
-  await blog.remove();
-
-  return blog._id.toString();
-};
-
 const blogsInDb = async () => {
   const blogs = await Blog.find({});
   return blogs.map((blog) => blog.toJSON());
 };
 
-module.exports = { initialBlogs, nonExistingId, blogsInDb };
+module.exports = { initialBlogs, blogsInDb };
